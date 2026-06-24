687dcccfbcc148c4b47803b7a24acb0f210a0c90
zonic-ultra <zonicultra@gmail.com>
2026-06-24 16:36:07 +0800

    scass

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // allowedDevOrigins: ["192.168.20.8"],
-  allowedDevOrigins: ["192.168.20.8", "http://192.168.20.8:3000"], // [CHANGED] added full origin
-
   reactCompiler: true,
   turbopack: {},
-
-  experimental: {
-    serverActions: {
-      allowedOrigins: ["192.168.20.8:3000"],
-    },
-  },
 };
 
 export default nextConfig;
