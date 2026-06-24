4fc109c7ba66c8280cea296773de9fec25bef890
zonic-ultra <zonicultra@gmail.com>
2026-06-24 17:31:13 +0800

    fix project service

--- src/services/project.service.ts
+++ src/services/project.service.ts
@@ -3,7 +3,7 @@ import { BaseService } from "./base.service";
 
 export class ProjectService extends BaseService<TProject> {
   constructor() {
-    super("/p/projects");
+    super("/projects");
   }
 
   async createProject(data: TCreateProject) {
