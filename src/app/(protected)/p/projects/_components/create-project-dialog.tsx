1913a7bc951ed07b8f445ac029ffd8ec7d6e03b1
zonic-ultra <zonicultra@gmail.com>
2026-07-03 10:34:59 +0800

    remove e

--- src/app/(protected)/p/projects/_components/create-project-dialog.tsx
+++ src/app/(protected)/p/projects/_components/create-project-dialog.tsx
@@ -72,7 +72,7 @@ export const CreateDialogProject = ({
         <form onSubmit={form.handleSubmit((data) => mutate(data))}>
           <DialogHeader>
             <DialogTitle>Create Project</DialogTitle>
-            <DialogDescription>Select Date Rangee</DialogDescription>
+            <DialogDescription>Select Date Range</DialogDescription>
           </DialogHeader>
 
           <FieldGroup>
