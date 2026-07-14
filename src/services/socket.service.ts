85682a9ae96988e3fa82ad8f7e53f712b019872b
zonic-ultra <zonicultra@gmail.com>
2026-07-14 17:33:54 +0800

    add notification v2

--- src/services/socket.service.ts
+++ src/services/socket.service.ts
@@ -1,18 +1,19 @@
 // services/socket.service.ts
 import { io, Socket } from "socket.io-client";
 
+// services/socket.service.ts
 class SocketService {
   private socket: Socket | null = null;
 
   connect(token: string): Socket {
     if (this.socket?.connected) return this.socket;
 
-    this.socket = io(`${process.env.NEXT_PUBLIC_WS_URL}/ws`, {
-      auth: { access_token: token },
+    // services/socket.service.ts
+    this.socket = io(`${process.env.NEXT_PUBLIC_WS_URL}/tasks-notifications`, {
+      auth: { accessToken: token },
       transports: ["websocket"],
       autoConnect: true,
     });
-
     return this.socket;
   }
 
