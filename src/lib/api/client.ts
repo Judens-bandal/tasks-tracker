0c200bb081abbc2a5e7f3bf570eeea9e39560429
zonic-ultra <zonicultra@gmail.com>
2026-07-15 14:25:58 +0800

    fix ngrok test

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -39,6 +39,7 @@ export const apiClient = axios.create({
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
+    "ngrok-skip-browser-warning": "true", // ← add this
   },
 });
 
